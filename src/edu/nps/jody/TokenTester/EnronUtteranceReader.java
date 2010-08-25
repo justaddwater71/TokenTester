050adb4d2a563c52ed34a547f67ba37bebcfd0f1
Jody <justaddwater71@gmail.com>
2010-08-24 19:49:46 -0700

    Added OneToOne file processing for Enron processing

--- src/edu/nps/jody/TokenTester/EnronUtteranceReader.java
+++ src/edu/nps/jody/TokenTester/EnronUtteranceReader.java
@@ -42,6 +42,35 @@ public class EnronUtteranceReader
 	}
 	
 	//Methods
+	public void recurseUtteranceFiles(String fileName, boolean oneForOne)
+	{
+		File file = new File(fileName);
+		String[] fileList;
+		//FIXME This is for initial monitoring only, remove once code is functioning
+		System.out.println("Evaluating " + fileName);
+		
+		if (file.isDirectory())
+		{
+			fileList = file.list();
+			
+			String absolutePath = file.getAbsolutePath();;
+			
+			for (int i = 0; i < fileList.length;i++)
+			{
+				fileList[i] = absolutePath +"/" +  fileList[i];
+			}
+			
+			for (int j = 0; j < fileList.length;j++)
+			{
+				processUtteranceFile(fileList[j]);
+			}
+		}
+		else if (file.isFile())
+		{
+			processUtteranceFile(file, oneForOne);
+		}
+	}
+	
 	public void recurseUtteranceFIles(String fileName)
 	{
 		File file = new File(fileName);
@@ -77,6 +106,78 @@ public class EnronUtteranceReader
 		//FIXME This is for initial monitoring only, remove once code is functioning
 		System.out.println("Processing " + utteranceFile.getName());
 		processUtteranceFile(utteranceFile);
+	}
+	
+	public void processUtteranceFile(File utteranceFile, boolean oneForOne)
+	{
+		String text = "";
+		String line = "";		
+		String svmText;
+		HashMap<String, Integer> stringMap;
+		HashMap<Integer, Integer> integerMap;
+		File libSVMTextFile = new File(utteranceFile.getName() + ".svm");
+		PrintWriter localLibSVMTextWriter;
+		
+		try 
+		{
+			localLibSVMTextWriter = new PrintWriter(libSVMTextFile);
+			reader					= new FileReader(utteranceFile);
+		} 
+		catch (FileNotFoundException e) 
+		{
+			//Bad file, jump out of this method
+			return;
+		}
+		
+		bufferedReader 	= new BufferedReader(reader);
+		
+		try {
+			while ((line = bufferedReader.readLine()) != null)
+			{
+				if (line.startsWith("newutterance"))
+				{
+					if (!text.equalsIgnoreCase(""))
+					{
+						System.out.println(line);
+						userName = utteranceFile.getName();//FIXME This will not work anywhere but in ENRON corpus.  Generalize when porting code over for Android email and text.
+						stringMap = FeatureMaker.textToFeatureMap(text, MAXGAP, null, FeatureMaker.FEATURE_OSB, wordTokenizer);
+						integerMap = textToSVM.StringMapToIntegerMap(stringMap, membershipChecker);
+						svmText = textToSVM.mapToString(userName, integerMap);
+						localLibSVMTextWriter.println(svmText);
+						line = "";
+						text = "";
+					}
+					else
+					{
+						line = "";
+					}
+				}
+				else
+				{
+					text = text + line;
+				}
+			}
+			
+			if (!text.equalsIgnoreCase(""))
+			{
+				System.out.println(line);
+				userName = utteranceFile.getName();//FIXME This will not work anywhere but in ENRON corpus.  Generalize when porting code over for Android email and text.
+				stringMap = FeatureMaker.textToFeatureMap(text, MAXGAP, null, FeatureMaker.FEATURE_OSB, wordTokenizer);
+				integerMap = textToSVM.StringMapToIntegerMap(stringMap, membershipChecker);
+				svmText = textToSVM.mapToString(userName, integerMap);
+				localLibSVMTextWriter.println(svmText);
+				line = "";
+				text = "";
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		localLibSVMTextWriter.flush();
+		localLibSVMTextWriter.close();
+		
+		return;
 	}
 	
 	
